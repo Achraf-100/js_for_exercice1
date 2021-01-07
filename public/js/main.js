d237bb9e9c05fe05fd331ae0d45e5687d344b1cc
Achraf <hardy1348@live.fr>
2021-01-07 16:52:25 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,35 @@
+// for (let i = 0; i < 20; i++){
+//     console.log(i);
+//     switch (i % 2 == 0 ) {
+//         case true:
+//             console.log('Nombre pair');
+//             break;
+//         case false:
+//             console.log('Nombre impair');
+//             break;
+//     }
+// }
+
+
+
+
+// let tab = ['Zoro' , 'luffy' , 'Sanji'] 
+
+// for (let index = 2; index < tab.length; index++) {
+//     console.log(`Bonjour ${tab[0]}`);
+//     console.log(`Bonjour ${tab[1]}`);
+//     console.log(`Bonjour ${tab[2]}`);
+// }
+
+
+
+
+// let nombre = parseInt(prompt('Insere un nombre'));
+
+// for (let index = 0; index <= nombre ; index++) {
+//     console.log(index);
+// }
+
+
+
+
